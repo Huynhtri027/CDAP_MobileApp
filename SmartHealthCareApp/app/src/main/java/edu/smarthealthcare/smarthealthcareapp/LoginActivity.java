563bdc6131c814b04c55bbc6a52fcaba42f06fee
sharaafnazeer <sharaaf.na@retailgenius.lk>
2017-07-09 16:43:56 +0530

    Commit 3 - Login Activity

--- SmartHealthCareApp/app/src/main/java/edu/smarthealthcare/smarthealthcareapp/LoginActivity.java
+++ SmartHealthCareApp/app/src/main/java/edu/smarthealthcare/smarthealthcareapp/LoginActivity.java
@@ -1,16 +1,40 @@
 package edu.smarthealthcare.smarthealthcareapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends Activity {
+
+    private Button btnLogin;
+    private TextView txtRegisterAcc;
+    private EditText txtUsername;
+    private EditText txtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        txtUsername = (EditText) findViewById(R.id.txtUsername);
+        txtPassword = (EditText) findViewById(R.id.txtUserPassword);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplication(), MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
